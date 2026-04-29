8f61837a974a6588447f5577c546df648045f2ff
Mohamad Arif <125123688+mohamadarif03@users.noreply.github.com>
2026-04-29 11:46:49 +0700

    push

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,23 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use App\Services\GeminiService;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
-
+    /**
+     * Register any application services.
+     */
     public function register(): void
     {
-        $this->app->singleton(GeminiService::class, function () {
-            return new GeminiService();
-        });
+        //
     }
+
     /**
      * Bootstrap any application services.
      */
     public function boot(): void
     {
-        //
+        URL::forceScheme('https'); 
     }
 }
